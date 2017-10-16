db9281fbfe58ffff3c92e9f97ffab500dc016d18
Matthew McGonagle <mwmcgonagle@gmail.com>
2017-10-16 17:02:32 -0400

    Added benchmarks for other list comprehension. Also added benchmarks for weights depending on the sub-square. Result is that usually some list comprehension outperforms index method, but it is possible for index method to do better.

--- assets/2017-10-13-SubsquaresNumpy.py
+++ assets/2017-10-13-SubsquaresNumpy.py
@@ -1,6 +1,7 @@
 import numpy as np
 import timeit 
 import random
+import matplotlib.pyplot as plt
 
 # First construct the array we will select sub-squares from.
 
@@ -55,6 +56,9 @@ subsquareSums = np.tensordot(subsquares, weights, axes = [[2, 3], [0, 1]])
 print(subsquareSums.shape)
 print(subsquareSums)
 
+
+# Functions for benchmarks of different methods.
+
 def indexingMethod(X, weights, sidel):
 
     nRows, nCols = X.shape
@@ -74,28 +78,130 @@ def listComprehensionMethod(X, weights, sidel):
                        for i in np.arange(nRows - sidel + 1) ]
     return np.array(subsquareSums)
 
+def listComprehensionMethod2(X, weigths, sidel):
+    nRows, nCols = weights.shape
+    xRows, xCols = X.shape
+    result = np.zeros((xRows - sidel + 1, xCols - sidel + 1))
+    for i in range(nRows):
+        for j in range(nCols):
+            result += X[i : i + xRows - sidel + 1, j : j + xCols - sidel + 1] * weights[i, j]
+    return result
+
+# Get benchmarks for different methods.      
+random.seed(1013)  
 B = [[random.randint(0, 10) for j in range(100)]
         for i in range(100)]
 B = np.array(B)
-sidel = 3
-weights = [[random.randint(-2, 2) 
-                for j in range(sidel) ]
-                for i in range(sidel) ]
 
-print("\nbenchmarks for sidel = ", sidel)
-print("indexingMethod benchmark = ")
-print(timeit.timeit(lambda : indexingMethod(B, weights, sidel), number = 50))
-print("listComprehensionMethod benchmark = ")
-print(timeit.timeit(lambda : listComprehensionMethod(B, weights, sidel), number = 50))
+results = {'indexMethod' : [], 'list1' : [], 'list2' : []}
+search = [3, 25, 50, 75, 97]
+for sidel in search: 
 
-sidel = 75
-weights = [[random.randint(-2, 2) 
-                for j in range(sidel) ]
-                for i in range(sidel) ]
+    weights = [[random.randint(-2, 2) 
+                    for j in range(sidel) ]
+                    for i in range(sidel) ]
+    weights = np.array(weights)
+    
+    print("\nbenchmarks for sidel = ", sidel)
 
-print("\nbenchmarks for sidel = ", sidel)
-print("indexingMethod benchmark = ")
-print(timeit.timeit(lambda : indexingMethod(B, weights, sidel), number = 50))
-print("listComprehensionMethod benchmark = ")
-print(timeit.timeit(lambda : listComprehensionMethod(B, weights, sidel), number = 50))
+    benchmark = timeit.timeit(lambda : indexingMethod(B, weights, sidel), number = 50)
+    print("indexingMethod benchmark = ", benchmark)
+    results['indexMethod'].append(benchmark)
+
+    benchmark = timeit.timeit(lambda : listComprehensionMethod(B, weights, sidel), number = 50)
+    print("listComprehensionMethod benchmark = ", benchmark)
+    results['list1'].append(benchmark)
+
+    benchmark = timeit.timeit(lambda : listComprehensionMethod2(B, weights, sidel), number = 50)
+    print("listComprehensionMethod2 benchmark = ", benchmark)
+    results['list2'].append(benchmark)
+
+    print('Check index method == list comprehension 1 : ', end = '')
+    print(np.array_equal(indexingMethod(B, weights, sidel), listComprehensionMethod(B, weights, sidel)))
+    print('Check index method == list comprehension 2 : ', end = '')
+    print(np.array_equal(indexingMethod(B, weights, sidel), listComprehensionMethod2(B, weights, sidel)))
+
+
+    
+for key in results:
+    results[key] = np.array(results[key])
+
+plt.plot(search, results['indexMethod'])
+plt.plot(search, results['list1'])
+plt.plot(search, results['list2'])
+plt.legend(['index', 'listComprehension1', 'listComprehension2'])
+plt.title('Benchmarks vs Different Sub-square Side Lengths')
+plt.savefig('2017-10-13-Benchmarks.svg')
+
+# Now look at benchmarks for weights that depend on sub-square.
+
+def indexingMethod(X, weights, sidel):
+
+    nRows, nCols = X.shape
+    cornersRow =  np.arange(nRows - sidel + 1)[:, np.newaxis, np.newaxis, np.newaxis]
+    cornersCol = np.arange(nCols - sidel + 1)[np.newaxis, :, np.newaxis, np.newaxis]
+
+    subsquareRow = cornersRow + np.arange(sidel)[:, np.newaxis]
+    subsquareCol = cornersCol + np.arange(sidel)
+    subsquares = X[subsquareRow, subsquareCol]
+    subsquareSums = np.sum(subsquares * weights, axis = (2, 3)) 
+    return subsquareSums
+
+def listComprehensionMethod(X, weights, sidel):
+    nRows, nCols = X.shape
+    subsquareSums = [[ np.tensordot(X[i : i + sidel, j : j + sidel], weights[i, j], [[0, 1], [0, 1]])
+                       for j in np.arange(nCols - sidel + 1) ] 
+                       for i in np.arange(nRows - sidel + 1) ]
+    return np.array(subsquareSums)
+
+def listComprehensionMethod2(X, weigths, sidel):
+    sqRos, sqCols, nRows, nCols = weights.shape
+    xRows, xCols = X.shape
+    result = np.zeros((xRows - sidel + 1, xCols - sidel + 1))
+    for i in range(nRows):
+        for j in range(nCols):
+            result += X[i : i + xRows - sidel + 1, j : j + xCols - sidel + 1] * weights[:, :, i, j]
+    return result
+
+
+results = {'indexMethod' : [], 'list1' : [], 'list2' : []}
+search = [3, 25, 35, 50, 75, 97]
+for sidel in search: 
+
+    weights = [[random.randint(-2, 2) 
+                    for j in range(sidel) ]
+                    for i in range(sidel) ]
+    weights = np.array(weights)
+    weights = weights + np.arange(100 - sidel + 1)[:, np.newaxis, np.newaxis]
+    weights = weights - np.arange(100 - sidel + 1)[:, np.newaxis, np.newaxis, np.newaxis]
+
+    
+    print("\nbenchmarks for sidel = ", sidel)
+
+    benchmark = timeit.timeit(lambda : indexingMethod(B, weights, sidel), number = 50)
+    print("indexingMethod benchmark = ", benchmark)
+    results['indexMethod'].append(benchmark)
+
+    benchmark = timeit.timeit(lambda : listComprehensionMethod(B, weights, sidel), number = 50)
+    print("listComprehensionMethod benchmark = ", benchmark)
+    results['list1'].append(benchmark)
+
+    benchmark = timeit.timeit(lambda : listComprehensionMethod2(B, weights, sidel), number = 50)
+    print("listComprehensionMethod2 benchmark = ", benchmark)
+    results['list2'].append(benchmark)
+
+   
+    print('Check index method == list comprehension 1 : ', end = '')
+    print(np.array_equal(indexingMethod(B, weights, sidel), listComprehensionMethod(B, weights, sidel)))
+    print('Check index method == list comprehension 2 : ', end = '')
+    print(np.array_equal(indexingMethod(B, weights, sidel), listComprehensionMethod2(B, weights, sidel)))
+
+
+plt.clf()
+plt.plot(search, results['indexMethod'])
+plt.plot(search, results['list1'])
+plt.plot(search, results['list2'])
+plt.legend(['index', 'listComprehension1', 'listComprehension2'])
+plt.title('Variable Weights Benchmarks vs Different Sub-square Side Lengths')
+plt.savefig('2017-10-13-VarWeightsBenchmarks.svg')
 
