09a2819f2a10561e8ca86b22b218db7636ad49bc
Matthew McGonagle <mwmcgonagle@gmail.com>
2017-11-26 16:35:08 -0500

    Added binomial table creation, random variable class, and methods to create a binomially distributed random variable from the binomial table. The binomial table is created in a dynamic programming manner. Instances of the random variable class are capable of being added to together to create a new instance representing the random variable created by their sum.

--- assets/2017-11-21-randomTraversal.py
+++ assets/2017-11-21-randomTraversal.py
@@ -82,11 +82,91 @@ class Tree:
             dataList.append(thisLevelData)
             nodeList = nextLevelNodes
 
+class RandomVar:
+
+    def __init__(self, values, probs):
+        if len(values) > 0:
+            self.values = values
+        else:
+            print( "ERROR: values is empty. RandomVar must take on values")
+
+        if len(probs) > 0:
+            self.probs = probs
+        else:
+            print( "ERROR: probs is empty. RandomVar must take on probabilies")
+
+    def add(self, randVar2):
+        newprobs = {}
+        for val, prob in zip(self.values, self.probs):
+            for val2, prob2 in zip(randVar2.values, randVar2.probs):
+                newval = val + val2
+                newprob = prob * prob2 
+                if newval in newprobs.keys():
+                    newprobs[newval] += newprob
+                else:
+                    newprobs[newval] = newprob 
+        newvals = np.array(list(newprobs.keys()))
+        newprobs = np.array(list(newprobs.values()))
+        return RandomVar(newvals, newprobs)
+
+class BinomialTable:
+
+    def __init__(self, n):
+        self.n = n
+        self.__computeTable(n)
+
+    def __computeTable(self, n):
+
+        self.table = np.zeros((n + 1, n + 1))
+        self.table[0,0] = 1
+        if n == 0:
+            return 
+        # Now we use the fact that C(n, k) = C(n-1, k) + C(n - 1, k - 1). This fact comes from determing
+        # the two cases of whether the last element is in the k-subset.
+        for nballs in range(1, n + 1):
+          self.table[nballs, 0] = 1
+          self.table[nballs, 1:] = self.table[nballs - 1, 1:] + self.table[nballs - 1, :-1] 
+
+    def getTable(self):
+        return self.table
+
+    def C(n, k):
+        if n < 0 or n > self.n:
+            print( "ERROR: n not in bounds for binomial table")
+            return 
+
+        elif k < 0 or k > self.n:
+            print( "ERROR: k not in bounds for binomial table")
+            return 
+
+        else:
+            return table[n, k]
+
+    def getRandomVar(self, n, p):
+        if n > self.n:
+            print("ERROR: n is too large for binomial table when creating random binomial variable")
+        q = 1 - p
+        vals = np.arange(n+1) 
+        probs = self.table[n, :n+1]
+        probs *= p**vals
+        probs *= q**(n - vals) 
+        return RandomVar(vals, probs)
+
+################### Start of main execution #############
+
 nLevels = 4
 nTraversals = 1000
+binomTable = BinomialTable(nLevels)
 np.random.seed(20171121)
 statTraversals = Tree(nLevels)
 
+print(binomTable.getTable())
+randVar1 = binomTable.getRandomVar(1, 0.5)
+print(randVar1.values, randVar1.probs)
+randVar2 = binomTable.getRandomVar(2, 0.5) 
+print(randVar2.values, randVar2.probs)
+randVar3 = randVar1.add(randVar2)
+print(randVar3.values, randVar3.probs)
 printI = 0
 for i in range(nTraversals):
 
