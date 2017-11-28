c9ee652799240cc591d11531b610c9fcf2e19706
Matthew McGonagle <mwmcgonagle@gmail.com>
2017-11-28 16:09:25 -0500

    Added comments, renamed Tree class to SimulatedTree, and moved SimulatedTree.__getDataList() to inside SimulatedTree.getDataList().

--- assets/2017-11-21-randomTraversal.py
+++ assets/2017-11-21-randomTraversal.py
@@ -3,30 +3,129 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm 
 import matplotlib.colors as colors
 
+# Class : Node
+# This class is used to hold data in a node of a binary tree. It has an initializer for its data
+# which defaults to None. Furthermore, its left and right children initialize as None.
+
 class Node:
+    '''
+    Class for nodes in tree.
+
+    Members
+    -------
+    self.data : Any type.
+        The data held by this node.
+    self.left : None or class Node.
+        Holds reference to the left child. Should be set to None if there is no left child.
+    self.right : None or class Node.
+        Holds reference to the right child. Should be set to None if there is no right child.
+     '''
 
     def __init__(self, data = None):
+        '''
+        Initializer.
+
+        Left and right children are defaulted to being None. The data held at the node may be passed
+        as a parameter, but if nothing is passed then self.data = None.
+        
+        Parameters
+        ----------
+        self : 
+            Reference to this node that is always passed.
+        data : Any type.
+            The data for this node to hold, defaults to None.
+        '''
+
         self.data = data
         self.left = None 
         self.right = None 
 
-class Tree:
+class SimulationTree:
+    '''
+    Class for running simulation of random traversal of binary tree.
+
+    Class sets up nodes for simulation of random traversal of binary tree. Each node holds a numpy array
+    of counts that are recorded while running simulation. The tree is of specified number of levels
+    (i.e. height) and is full. That is, for a specified number of levels, the tree contains all of its nodes.
+
+    Member Variables
+    ----------------
+    nNodes : int
+        The number of nodes in the tree.
+    maxLevel : int
+        The number identifying the largest level the tree. The root is at level 0.
+    root : Node
+        The root node of the tree.
+    __rollValue : dict
+        The values of random rolls to use to determine whether to use a preorder traversal, inorder traversal,
+        or postorder traversal.
+    '''
 
     def __init__(self, nLevels):
+        '''
+        Initializer.
+
+        Sets up all of the nodes in the tree and all of the member variables.
+    
+        Parameters
+        ----------
+        nLevels : int
+            The number of levels in the tree, including the root. So if nLevels is 1, then the tree consists
+            of only the root.
+        '''
         self.nNodes = 2**nLevels - 1
         self.maxLevel = nLevels - 1
         self.root = Node(np.zeros(self.nNodes))
-        self.addChildren(0, self.root)
-        self.rollValue = {'preorder' : 0, 'inorder' : 1, 'postorder' : 2}
 
-    def addChildren(self, level, node):
+        # Create subtrees of root.
+        self.__addChildren(0, self.root)
+
+        self.__rollValue = {'preorder' : 0, 'inorder' : 1, 'postorder' : 2}
+
+    def __addChildren(self, level, node):
+        '''
+        Creates subtrees by adding children.
+
+        Continues recursively adding children nodes until we are at one level below the maximum level.
+
+        Parameters
+        ----------
+        self : self
+        level : int
+            The level of the current node. The root should start at 0.
+        node : Node
+            The current node to add children to.        
+        '''
         node.left = Node(np.zeros(self.nNodes))
         node.right = Node(np.zeros(self.nNodes))
+
+        # Check if we still need to add children.
         if level < self.maxLevel - 1:
-            self.addChildren(level + 1, node.left) 
-            self.addChildren(level + 1, node.right)
+            self.__addChildren(level + 1, node.left) 
+            self.__addChildren(level + 1, node.right)
 
     def __randTraversal(self, n, node):
+        '''
+        Perform random traversal decision at node.
+        
+        Randomly decide whether to do a preorder, inorder, or postorder traversal decision at this node. 
+        For the next node we process, we will call it n (that is add +1 to the n statistic at that node).
+        For a preorder, that will be this node. For inorder and postorder, the next node processed will be in the
+        left subtree (if non-empty). However, for inorder, this node will be processed after the left sub-tree;
+        for postorder, this node will be processed after the right sub-tree. 
+
+        Parameters
+        ----------
+        self : self
+        n : int
+            We will call the next processed node n.
+        node : Node
+            The current node to which we make a random traversal decision. 
+
+        Returns
+        -------
+        After processing this node, the number to mark the next node processed.
+        '''
 
         # For empty leaves, don't do anything.
 
@@ -37,37 +136,64 @@ class Tree:
 
         diceroll = np.random.randint(0, 3)
 
-        if diceroll == self.rollValue['preorder']:
+        # For roll of preorder, process this node, then the left sub-tree, and then the right sub-tree. 
+
+        if diceroll == self.__rollValue['preorder']:
             node.data[n] += 1
             newN = self.__randTraversal(n+1, node.left)
             newN = self.__randTraversal(newN, node.right) 
             return newN
 
-        elif diceroll == self.rollValue['inorder']:
+        # Else, if we roll inorder traversal, then process left sub-tree, this node, and then the
+        # the right sub-tree.
+
+        elif diceroll == self.__rollValue['inorder']:
             newN = self.__randTraversal(n, node.left)
             node.data[newN] += 1 
             newN = self.__randTraversal(newN + 1, node.right)
             return newN
 
-        else: # post order case
+        # Else we have a post order roll. So we process left sub-tree, right sub-tree, and then this node.
+        else: 
             newN = self.__randTraversal(n, node.left)
             newN = self.__randTraversal(newN, node.right)
             node.data[newN] += 1
             return newN + 1
 
     def randTraversal(self):
+        '''
+        Do a random traversal of the entire tree starting at the root.
+
+        We will mark each node from 0 to self.nNodes - 1. However, we do this processing in a random order.
+        At each node, we randomly decide to process this node in either a preorder, inorder, or postorder
+        fashion. Since we are interested in the statistics, we increment the count of that number at the node
+        contained in the node.data array. This function will only do one traversal, incrementing one single
+        statistic for each node.
+
+        Parameters
+        ----------
+        self : self
+        '''
         checkN = self.__randTraversal(0, self.root)
 
     def getDataList(self):
+        '''
+        Gets statistics of all the nodes.
+
+        Groups the statistics of the nodes into lists for each level. So we get a list of 2d numpy arrays.
+
+        Parameters
+        ----------
+        self : self
+
+        Returns
+        -------
+        A list of 2d numpy arrays where each 2d numpy array holds the statistics of all the nodes in one level.
+        That is, each 2d numpy array has shape (number of nodes in level, number of nodes in tree).
+        '''
+
         dataList = []
-        self.__getDataList(dataList, [self.root])
-        return dataList
-
-
-    # In order traversal
-
-    def __getDataList(self, dataList, nodeList):
-
+        nodeList = [self.root]
         while(nodeList != []):
 
             nextLevelNodes = []
@@ -84,6 +210,8 @@ class Tree:
 
             dataList.append(thisLevelData)
             nodeList = nextLevelNodes
+
+        return dataList
 
 class RandomVar:
 
@@ -247,7 +375,7 @@ binomTable = BinomialTable(nLevels)
 np.random.seed(20171121)
 model = modelTree(nLevels)
 modelList = model.getDataList()
-statTraversals = Tree(nLevels)
+statTraversals = SimulationTree(nLevels)
 
 print(binomTable.getTable())
 print(binomTable.getRandomVar(3, 0.25).values, binomTable.getRandomVar(3, 0.25).probs)
